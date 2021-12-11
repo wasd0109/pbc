a2f870d39ee67687a18342a83662a4f492138b2b
Ken Cheung <wasd0109.dev@gmail.com>
2021-12-11 20:23:30 +0900

    Add online only filter

--- pages/search.vue
+++ pages/search.vue
@@ -8,10 +8,16 @@
       v-model="searchText"
     >
     </v-text-field>
+    <v-checkbox v-model="isOnlineOnly" label="Online Only"></v-checkbox>
     <v-list>
       <v-list-item v-for="event of filteredList">
         <v-list-item-avatar
-          ><v-img :src="event['Event Image']"></v-img
+          ><v-img
+            :src="
+              event['Event Image'] ||
+              'https://www.iapco.org/app/plugins/events-calendar-pro/src/resources/images/tribe-related-events-placeholder.png'
+            "
+          ></v-img
         ></v-list-item-avatar>
         <v-list-item-title>{{ event.Title }}</v-list-item-title>
         <v-list-item-action
@@ -34,37 +40,58 @@ export default {
     this.eventList = response.results;
     this.filteredList = response.results;
   },
+  methods: {
+    filterEventList(searchFilter, searchText, isOnlineOnly) {
+      let filteredList = this.eventList;
+      if (searchText) {
+        if (searchFilter !== "Tags") {
+          filteredList = this.eventList.filter((event) =>
+            event[searchFilter].toLowerCase().includes(searchText.toLowerCase())
+          );
+        } else {
+          filteredList = this.eventList.filter((event) => {
+            const eventTags = event["Tag"];
+            for (let eventTag of eventTags) {
+              if (
+                eventTag.toLowerCase().includes(this.searchText.toLowerCase())
+              ) {
+                return true;
+              }
+              return false;
+            }
+          });
+        }
+      }
+      if (isOnlineOnly) {
+        filteredList = filteredList.filter((event) => event["Is Online"]);
+      }
+      return filteredList;
+    },
+  },
   data() {
     return {
       eventList: [],
       filteredList: [],
       searchText: "",
-      searchFilterItems: ["Title", "Tags", "Description"],
+      searchFilterItems: ["Title", "Tags"],
       searchFilter: "Title",
+      isOnlineOnly: false,
     };
   },
   watch: {
     searchText() {
-      if (this.searchFilter !== "Tags") {
-        this.filteredList = this.eventList.filter((event) =>
-          event[this.searchFilter]
-            .toLowerCase()
-            .includes(this.searchText.toLowerCase())
-        );
-      } else {
-        this.filteredList = this.eventList.filter((event) => {
-          const eventTags = event["Tag"];
-          for (let eventTag of eventTags) {
-            if (
-              eventTag.toLowerCase().includes(this.searchText.toLowerCase())
-            ) {
-              return true;
-            }
-            return false;
-          }
-        });
-      }
-      console.log(this.filteredList);
+      this.filteredList = this.filterEventList(
+        this.searchFilter,
+        this.searchText,
+        this.isOnlineOnly
+      );
+    },
+    isOnlineOnly() {
+      this.filteredList = this.filterEventList(
+        this.searchFilter,
+        this.searchText,
+        this.isOnlineOnly
+      );
     },
   },
 };
