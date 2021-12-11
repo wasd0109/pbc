77eca34f9bc0983ffaa39dd0cbe84d26b131c643
Ken Cheung <wasd0109.dev@gmail.com>
2021-12-11 14:57:30 +0900

    Add tag chip selection and confirm tag API

---
+++ components/Chips.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-chip @click="toggleSelected" :color="selected ? 'green' : ''">{{
+    tag.Name
+  }}</v-chip>
+</template>
+
+<script>
+export default {
+  props: ["tag"],
+  data() {
+    return {
+      selected: false,
+    };
+  },
+  methods: {
+    toggleSelected() {
+      if (this.selected) {
+        this.$emit("deselected", this.$props.tag);
+        this.selected = false;
+      } else {
+        this.$emit("selected", this.$props.tag);
+        this.selected = true;
+      }
+    },
+  },
+  emits: ["selected", "deselected"],
+};
+</script>
+
+<style scoped></style>
