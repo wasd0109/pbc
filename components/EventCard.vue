96e1b80b420963727b75c551b7cbd0e7b361e354
Ken Cheung <wasd0109.dev@gmail.com>
2021-12-11 20:31:21 +0900

    Add image to EventCard

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -2,7 +2,14 @@
   <v-card min-width="100%">
     <v-container
       ><nuxt-link :to="`/event/${event._id}`" nuxt as="div">
-        <v-img src="http://placehold.jp/200x100.png"></v-img>
+        <v-img
+          :src="
+            event['Event Image'] ||
+            'https://www.iapco.org/app/plugins/events-calendar-pro/src/resources/images/tribe-related-events-placeholder.png'
+          "
+          min-height="400px"
+          max-height="400px"
+        ></v-img>
         <v-card-title>{{ event.Title }}</v-card-title>
         <v-card-subtitle>{{ event.Description }}</v-card-subtitle>
         <v-chip class="tag" v-for="tag of event.Tag" :key="tag"
