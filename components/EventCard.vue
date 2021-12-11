992f21f374b6b5d514b87416c8a52f344a3afed1
Ken Cheung <wasd0109.dev@gmail.com>
2021-12-11 20:38:10 +0900

    Add image to EventDetail page

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -1,22 +1,23 @@
 <template>
   <v-card min-width="100%">
-    <v-container
-      ><nuxt-link :to="`/event/${event._id}`" nuxt as="div">
-        <v-img
-          :src="
-            event['Event Image'] ||
-            'https://www.iapco.org/app/plugins/events-calendar-pro/src/resources/images/tribe-related-events-placeholder.png'
-          "
-          min-height="400px"
-          max-height="400px"
-        ></v-img>
+    <nuxt-link :to="`/event/${event._id}`" nuxt as="div">
+      <v-img
+        :src="
+          event['Event Image'] ||
+          'https://www.iapco.org/app/plugins/events-calendar-pro/src/resources/images/tribe-related-events-placeholder.png'
+        "
+        min-height="400px"
+        max-height="400px"
+      ></v-img>
+      <v-container>
         <v-card-title>{{ event.Title }}</v-card-title>
         <v-card-subtitle>{{ event.Description }}</v-card-subtitle>
         <v-chip class="tag" v-for="tag of event.Tag" :key="tag"
           ><p>{{ tag }}</p></v-chip
-        >
-      </nuxt-link>
-      <v-card-actions> <slot></slot> </v-card-actions></v-container
+        ></v-container
+      >
+    </nuxt-link>
+    <v-card-actions> <slot></slot> </v-card-actions
   ></v-card>
 </template>
 
